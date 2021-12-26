8336400bde694edad1be55487a55f87f414fc2aa
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-12-26 17:17:37 +0200

    added footer in progress

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,6 +1,7 @@
+import { v4 as uuid } from "uuid";
 import styles from "./List.module.css";
 
-export default function List({ array }) {
+export function List({ array }) {
   return (
     <ul className={styles.list}>
       {array.map(({ id, content, link }) => {
@@ -11,6 +12,16 @@ export default function List({ array }) {
             </a>
           </li>
         );
+      })}
+    </ul>
+  );
+}
+
+export function FooterList({ array }) {
+  return (
+    <ul>
+      {array.map((el) => {
+        return <li key={uuid()}>{el}</li>;
       })}
     </ul>
   );
