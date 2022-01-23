04e67377e87ab9e99e57107dc36d5ee323f493eb
Korotkova-Iuliia <3589900@gmail.com>
2022-01-23 16:59:54 +0200

    features

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/header/Header';
 import headerLinks from './headerLinks.json';
 import Hero from './components/hero/Hero';
-// import Feature from './components/';
+import Feature from './components/features/Features';
 import Footer from './components/footer/Footer';
 import Container from './components/container/Container';
 
@@ -12,6 +12,7 @@ function App() {
       <Container>
         <Header links={headerLinks} />
         <Hero />
+        <Feature />
         <Footer />
       </Container>
     </>
---
+++ src/components/features/Features.jsx
@@ -0,0 +1,18 @@
+import Icon from '../Icon/Icon';
+const Features = () => {
+  return (
+    <ul>
+      <li>
+        <Icon name="close" fill="red" />
+        <p>lorem</p>
+      </li>
+      <li>
+        <button type="button">
+          <Icon name="search" fill="green" width="50px" height="50px" />
+          <p>lorem</p>
+        </button>
+      </li>
+    </ul>
+  );
+};
+export default Features;
