491ff14cc7e4750b85256c3b5d36d64b5ff2c24c
Korotkova-Iuliia <3589900@gmail.com>
2022-01-23 16:23:30 +0200

    features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-import Header from "./components/header/Header";
-import Hero from "./components/hero/Hero";
-import headerLinks from "./headerLinks.json";
-import Footer from "./components/footer/Footer";
-import Container from "./components/container/Container";
+import './App.css';
+import Header from './components/header/Header';
+import headerLinks from './headerLinks.json';
+import Hero from './components/hero/Hero';
+// import Feature from './components/';
+import Footer from './components/footer/Footer';
+import Container from './components/container/Container';
 
 function App() {
   return (
