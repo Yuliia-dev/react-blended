db5c167080d40baf4443dcfc5589fb92fc590f74
AliVik <alvikul1994@gmail.com>
2022-02-06 17:04:57 +0200

    add routing

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,38 @@
+import { NavLink, Outlet } from "react-router-dom";
 import Features from "../components/features/Features.jsx";
 
+const array = [
+  {
+    id: "1",
+    title: "Oranges",
+  },
+  {
+    id: "2",
+    title: "Kiwi",
+  },
+  {
+    id: "3",
+    title: "Banana",
+  },
+  {
+    id: "4",
+    title: "Apple",
+  },
+];
 export default function AboutPage() {
-  return <Features />;
+  return (
+    <>
+      <Features />
+      <ul>
+        {array.map((el) => {
+          return (
+            <li key={el.id}>
+              <NavLink to={el.id}>{el.title}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+      <Outlet />
+    </>
+  );
 }
